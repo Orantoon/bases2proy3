5fb4744e217537b05f490e9027b866e43bdc9a8c
Deif <davidsuarez2001@gmail.com>
2023-06-16 00:03:09 -0600

    Cambios pequeños a los Scripts de Backups

--- helm/backups/scripts/postgresql.sh
+++ helm/backups/scripts/postgresql.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 DATE=$(date '+%Y%m%d%H%M')
-mkdir -p /postgresqlbackup/$DATE
+mkdir -p /postgresqldump/$DATE
 apk update
 apk upgrade
 apk add postgresql-client
 az config set extension.use_dynamic_install=yes_without_prompt
-pg_dump $PGDATABASE > /postgresqlbackup/$DATE/database.sql
-#pg_dump --host="$POSTGRESQL_VOLUME_DIR " -U $POSTGRESQL_USERNAME -d $POSTGRESQL_DATABASE --file=/postgresqlbackup/$DATE/database.sql
+pg_dump $PGDATABASE > /postgresqldump/$DATE/database.sql
 gzip /postgresqlbackup/$DATE/database.sql  # Compress the database dump file
 az storage blob directory upload --container $CONTAINER -s /postgresqlbackup/$DATE -d $BACKUP_PATH --auth-mode key --recursive
-rm -rf /postgresqlbackup/$DATE
+rm -rf /postgresqldump/$DATE
 
 
